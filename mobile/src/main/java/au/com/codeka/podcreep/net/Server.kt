f2c60af2e4f0673b867b4255f5d8850e4db9bf50
Dean Harding <dean@codeka.com.au>
2022-03-12 10:47:38 -0800

    Fixed the URL of the server.

--- mobile/src/main/java/au/com/codeka/podcreep/net/Server.kt
+++ mobile/src/main/java/au/com/codeka/podcreep/net/Server.kt
@@ -22,7 +22,7 @@ class Server {
       return when {
         isEmulator() -> "http://10.0.2.2:8080$url"
         BuildConfig.DEBUG -> "http://127.0.0.1:8080$url"
-        else -> "https://pod-creep.appspot.com$url"
+        else -> "https://podcreep.com$url"
       }
     }
 
