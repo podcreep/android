1c35af66b7f3feb4eb96d88fc74bed5abfe43980
Dean Harding <dean@codeka.com>
2024-09-14 21:35:47 -0700

    Some changes

--- mobile/src/main/java/com/podcreep/ui/Screen.kt
+++ mobile/src/main/java/com/podcreep/ui/Screen.kt
@@ -16,7 +16,6 @@ import java.util.*
  * A [Screen] is similar to a fragment, in that it's a place to keep the business logic
  * of a view.
  *
- *
  * Unlike a Fragment, though, a Screen's lifecycle is very simple. It's created, shown, hidden
  * and destroyed. Once created, it can be shown and hidden multiple times (for example as you
  * navigate the backstack, it might be hidden and then shown again).
